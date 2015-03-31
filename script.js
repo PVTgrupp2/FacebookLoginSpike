8c4bc38865d4c77ad254b19dbe4e07b0e5e83f98
Sarrgas <sargas1@hotmail.com>
2015-03-31 13:29:33 +0200

    Ändrade tillvägagångssätt för inlogg - använder nu jQuery

--- script.js
+++ script.js
@@ -1,39 +1,17 @@
-function fb_login(){
-    FB.login(function(response) {
-
-        if (response.authResponse) {
-            console.log('Welcome!  Fetching your information.... ');
-            //console.log(response); // dump complete info
-            access_token = response.authResponse.accessToken; //get access token
-            user_id = response.authResponse.userID; //get FB UID
-
-            FB.api('/me', function(response) {
-                user_email = response.email; //get user email
-          // you can store this data into your database  
-			document.getElementById('status').innerHTML = 'Welcome, ' + response.name + '!';		  
-            });
-        
-		} else {
-            //user hit cancel button
-            console.log('User cancelled login or did not fully authorize.');
-
-        }
-    }, {
-        scope: 'publish_stream,email'
+﻿$(document).ready(function(){
+	$('#fblogin').click(function(){
+		var anv = $.fblogin({fbId: '392795464233014'});
 		
-    });
+		anv.done(function(data){
+			console.log(data);
+			$('#status').text("Välkommen " + data.name + "!");
+		});
+	});
 	
-	
-}
+	$('#fblogout').click(function(){
+		FB.logout(function(response){
+			$('#status').text("Du loggades ut");
+		});
+	});
+});
 
-function fb_logout(){
-	FB.logout(function(response) {
-        // Person is now logged out
-    });
-}
-(function() {
-    var e = document.createElement('script');
-    e.src = document.location.protocol + '//connect.facebook.net/en_US/all.js';
-    e.async = true;
-    document.getElementById('fb-root').appendChild(e);
-}());
